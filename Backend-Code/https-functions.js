a47de6819121846fc3b3eb9b1c3211f03730c50d
AllyMikaels <103890902+AllyMikaels@users.noreply.github.com>
2022-07-08 17:52:59 -0400

    accident-copy wrong file-

--- Backend-Code/https-functions.js
+++ Backend-Code/https-functions.js
@@ -1,38 +1,46 @@
+import { response } from 'wix-http-functions';
 import { google } from 'googleapis';
+import { authentication } from 'wix-members-backend';
 import { getSecret } from 'wix-secrets-backend';
-import crypto from 'crypto'
+import { fetch } from 'wix-fetch';
 
-export async function getAuthUrl() {
- 
+export async function get_getAuth(request) {
+
  const googleClientSecret = await getSecret('Client1');
- 
  const googleConfig = {
   clientId: '967944416678-njio8l8bd2jpf88rj3a9g6balvjji150.apps.googleusercontent.com',
   clientSecret: googleClientSecret,
   redirect: 'https://www.gbccs.org/_functions/getAuth'
  };
 
- const authConnection = new google.auth.OAuth2(
-  googleConfig.clientId,
+ const code = await request.query.code
+ const state = await request.query.state
+
+ const auth = new google.auth.OAuth2(
   googleConfig.clientSecret,
+  googleConfig.clientId,
   googleConfig.redirect
  );
 
- const scope = [
-  'https://www.googleapis.com/auth/userinfo.email',
-  'https://www.googleapis.com/auth/userinfo.profile',
- ];
-  
-  const state = crypto.randomBytes(16).toString('hex')
+ const auth2 = await auth.getToken(code);
+ const tokens = auth2.tokens;
 
- const authUrl = authConnection.generateAuthUrl({
-  access_type: 'offline', 
-  prompt: 'consent',
-  scope: scope,
-  state: state
+ const userInfoRes = await fetch(`https://www.googleapis.com/oauth2/v2/userinfo?alt=json&access_token=${tokens.access_token}`, { "method": "get" })
+
+ if (!userInfoRes.ok) {
+  console.log("cound not get user info using access token")
+ }
+
+ const userInfo = (await userInfoRes.json())
+ const useremail = userInfo.email
+ const profilePicture = userInfo.picture
+
+ const sessiontoken = await authentication.generateSessionToken(useremail);
+
+ return response({
+  status: 302,
+  headers: { 'Location': `https://www.gbccs.org/signed-in?sessiontoken=${sessiontoken}&responseState=${state}&profilepic=${profilePicture}` }
  });
-
- return {state,authUrl};
 }
 
 console.log
